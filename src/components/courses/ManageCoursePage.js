87b1b93a24d880109a335180ab79d701941449b7
henri.lim <henri.lim@serko.com>
2020-07-05 22:15:10 +1200

    Complete chapter 11

---
+++ src/components/courses/ManageCoursePage.js
@@ -0,0 +1,94 @@
+import React, { useState, useEffect } from "react";
+import { connect } from "react-redux";
+import * as courseActions from "../../redux/actions/courseActions";
+import * as authorActions from "../../redux/actions/authorActions";
+import PropTypes from "prop-types";
+import CourseForm from "./CourseForm";
+import { newCourse } from "../../../tools/mockData";
+
+function ManageCoursePage({
+  courses,
+  authors,
+  loadCourses,
+  saveCourse,
+  loadAuthors,
+  history,
+  ...props
+}) {
+  const [course, setCourse] = useState({ ...props.course });
+  const [errors, setErrors] = useState({});
+
+  useEffect(() => {
+    if (courses.length === 0) {
+      loadCourses().catch((error) => {
+        alert("Loading courses failed " + error);
+      });
+    } else {
+      setCourse({ ...props.course });
+    }
+
+    if (authors.length === 0) {
+      loadAuthors().catch((error) => {
+        alert("Loading authors failed " + error);
+      });
+    }
+  }, [props.course]);
+
+  function handleChange(event) {
+    const { value, name } = event.target;
+    setCourse((prevCourse) => ({
+      ...prevCourse,
+      [name]: name === "authorId" ? parseInt(value, 10) : value,
+    }));
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    saveCourse(course).then(() => history.push("/courses"));
+  }
+
+  return (
+    <CourseForm
+      course={course}
+      errors={errors}
+      authors={authors}
+      onChange={handleChange}
+      onSave={handleSubmit}
+    />
+  );
+}
+
+ManageCoursePage.propTypes = {
+  course: PropTypes.object.isRequired,
+  courses: PropTypes.array.isRequired,
+  authors: PropTypes.array.isRequired,
+  loadCourses: PropTypes.func.isRequired,
+  saveCourse: PropTypes.func.isRequired,
+  loadAuthors: PropTypes.func.isRequired,
+  history: PropTypes.object.isRequired,
+};
+
+export function getCourseBySlug(courses, slug) {
+  return courses.find((course) => course.slug === slug) || null;
+}
+
+function mapStateToProps(state, ownProps) {
+  const slug = ownProps.match.params.slug;
+  const course =
+    slug && state.courses.length > 0
+      ? getCourseBySlug(state.courses, slug)
+      : newCourse;
+  return {
+    courses: state.courses,
+    authors: state.authors,
+    course,
+  };
+}
+
+const mapDispatchToProps = {
+  loadCourses: courseActions.loadCourses,
+  saveCourse: courseActions.saveCourse,
+  loadAuthors: authorActions.loadAuthors,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ManageCoursePage);
--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,11 +1,16 @@
 import * as types from "./actionTypes";
 import * as courseApi from "../../api/courseApi";
-export function createCourse(course) {
-  return { type: types.CREATE_COURSE, course };
-}
 
 export function loadCoursesSuccess(courses) {
   return { type: types.LOAD_COURSES_SUCCESS, courses };
+}
+
+export function saveCourseSuccess(course) {
+  return { type: types.SAVE_COURSE_SUCCESS, course };
+}
+
+export function createCourseSuccess(course) {
+  return { type: types.CREATE_COURSE_SUCCESS, course };
 }
 
 export function loadCourses() {
@@ -13,6 +18,21 @@ export function loadCourses() {
     return courseApi
       .getCourses()
       .then((courses) => dispatch(loadCoursesSuccess(courses)))
+      .catch((error) => {
+        throw error;
+      });
+  };
+}
+
+export function saveCourse(course) {
+  return (dispatch) => {
+    return courseApi
+      .saveCourse(course)
+      .then((savedCourse) =>
+        course.id
+          ? dispatch(saveCourseSuccess(savedCourse))
+          : dispatch(createCourseSuccess(savedCourse))
+      )
       .catch((error) => {
         throw error;
       });
